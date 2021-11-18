b83923931fd29f794603a934cdac902730cd7354
Andreas Kainz <kainz.a@gmail.com>
2021-11-18 01:43:29 +0100

    update git icon and project preference icon

--- addons/project/kateprojectconfigpage.cpp
+++ addons/project/kateprojectconfigpage.cpp
@@ -129,7 +129,7 @@ QString KateProjectConfigPage::fullName() const
 
 QIcon KateProjectConfigPage::icon() const
 {
-    return QIcon::fromTheme(QLatin1String("view-list-tree"));
+    return QIcon::fromTheme(QLatin1String("preferences-git"));
 }
 
 void KateProjectConfigPage::apply()
